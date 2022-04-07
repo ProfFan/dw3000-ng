026bcf4c4070cb77f544593604f808698820f415
thomas.raimbault <thomas.raimbault@sii.fr>
2022-04-07 12:02:11 +0200

    Nettoyage lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,6 @@
 //! [`dw1000`]: https://crates.io/crates/dw1000
 //! [`embedded-hal`]: https://crates.io/crates/embedded-hal
 
-#![no_std]
-#![no_main]
 #![deny(missing_docs)]
 
 pub mod configs;
@@ -53,39 +51,3 @@ pub use crate::{
 	},
 	block::block,
 };
-
-use defmt_rtt as _; // global logger
-
-// TODO(5) adjust HAL import
-use stm32f1xx_hal as _; // memory layout
-
-use panic_probe as _;
-
-// same panicking *behavior* as `panic-probe` but doesn't print a panic message
-// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
-#[defmt::panic_handler]
-fn panic() -> ! {
-    cortex_m::asm::udf()
-}
-
-/// Terminates the application and makes `probe-run` exit with exit-code = 0
-pub fn exit() -> ! {
-    loop {
-        cortex_m::asm::bkpt();
-    }
-}
-
-// defmt-test 0.3.0 has the limitation that this `#[tests]` attribute can only be used
-// once within a crate. the module can be in any file but there can only be at most
-// one `#[tests]` module in this library crate
-#[cfg(test)]
-#[defmt_test::tests]
-mod unit_tests {
-    use defmt::assert;
-
-    #[test]
-    fn it_works() {
-        assert!(true)
-    }
-}
-
